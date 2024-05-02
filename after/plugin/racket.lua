8a54664688a09f3f49e7b17d251c1a0049558bc3
Santiago Romero <srp.devel@gmail.com>
2024-05-01 19:06:09 -0600

    Tabs de Racket

--- after/plugin/racket.lua
+++ after/plugin/racket.lua
@@ -5,3 +5,13 @@ function MyDrRacket()
     -- char 21 will clear any input previously on the terminal
     vim.api.nvim_chan_send(vim.bo.channel, string.char(21) .. "racket -i -e '(enter! \"" .. title .. "\")'\r")
 end
+
+-- Special tabbing
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "racket",
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end
+})
